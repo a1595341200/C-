a60228d5a297ddffbeea162423a2a7877dee9b8f
谢瑶 <>
2023-02-26 13:30:55 +0800

    no message

---
+++ mac/myproject/build.sh
@@ -0,0 +1,11 @@
+###
+ # @Description: 
+ # @Author: yao.xie
+ # @Date: 2023-02-23 09:37:01
+ # @LastEditTime: 2023-02-23 09:37:02
+ # @LastEditors: yao.xie
+### 
+
+cmake -B build_linux
+cd build_linux
+make -j4
