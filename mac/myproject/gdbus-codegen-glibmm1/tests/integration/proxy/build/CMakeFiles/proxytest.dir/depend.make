6d48d31ed5495d8af0ffd45d506940ca8d754343
谢瑶 <>
2023-02-26 13:33:23 +0800

    移除无用文件

--- mac/myproject/gdbus-codegen-glibmm1/tests/integration/proxy/build/CMakeFiles/proxytest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for proxytest.
-# This may be replaced when dependencies are built.
--- mac/myproject/gdbus-codegen-glibmm1/tests/integration/proxy/build/generated/another-service_common.cpp
+++
@@ -1,4 +0,0 @@
-#include "/Users/xieyao/Desktop/git/C-/mac/myproject/gdbus-codegen-glibmm/tests/integration/proxy/build/generated/another-service_common.h"
-
-#include <cstring>
-
